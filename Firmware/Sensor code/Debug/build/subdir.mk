74f7a9b3b25458fb3b6556deadb658ecc0964888
GeekyRobo <80465811+Dichochi@users.noreply.github.com>
2024-05-16 17:12:06 +0200

    Sensor code upload

---
+++ Firmware/Sensor code/Debug/build/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ELF_SRCS += \
+../build/adc.elf 
+
+O_SRCS += \
+../build/adc.o \
+../build/gpio.o \
+../build/main.o \
+../build/startup_stm32f051x8.o \
+../build/stm32f0xx_hal.o \
+../build/stm32f0xx_hal_adc.o \
+../build/stm32f0xx_hal_adc_ex.o \
+../build/stm32f0xx_hal_cortex.o \
+../build/stm32f0xx_hal_dma.o \
+../build/stm32f0xx_hal_exti.o \
+../build/stm32f0xx_hal_flash.o \
+../build/stm32f0xx_hal_flash_ex.o \
+../build/stm32f0xx_hal_gpio.o \
+../build/stm32f0xx_hal_i2c.o \
+../build/stm32f0xx_hal_i2c_ex.o \
+../build/stm32f0xx_hal_msp.o \
+../build/stm32f0xx_hal_pwr.o \
+../build/stm32f0xx_hal_pwr_ex.o \
+../build/stm32f0xx_hal_rcc.o \
+../build/stm32f0xx_hal_rcc_ex.o \
+../build/stm32f0xx_hal_tim.o \
+../build/stm32f0xx_hal_tim_ex.o \
+../build/stm32f0xx_it.o \
+../build/system_stm32f0xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
